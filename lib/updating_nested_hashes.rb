002deab15b3cedd6c0f14dbbaab251d193e46bd2
Lida Attenborough <attenboroughl@protonmail.com>
2020-07-19 02:17:40 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -66,8 +66,8 @@ def add_characters
    # Code your solution here:
 
 epic_tragedy[:additional_characters] = []
-epic_tragedy[:additional_characters][0]{ :name => "Prince Escalus" }
-epic_tragedy[:additional_characters][1]{ :name => "Apothecary" }
+epic_tragedy[:additional_characters][0] = { :name => "Prince Escalus" }
+epic_tragedy[:additional_characters][1] = { :name => "Apothecary" }
 
 
 
